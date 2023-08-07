2a68ff8214bee4062e9de87ff76495c7f91f6281
MesutKizilay <mesutkizilay3868@gmail.com>
2023-08-07 17:26:43 +0300

    Dtos for category entity and repositories were added.

---
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		readonly ICategoryRepository _categoryRepository;
+		public CategoriesController(ICategoryRepository categoryRepository)
+		{
+			_categoryRepository = categoryRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetCategoryList()
+		{
+			var result = await _categoryRepository.GetAllCategoryAsync();
+			return Ok(result);
+		}
+	}
+}
---
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace RealEstate_Dapper_Api.Models.DapperContext
+{
+	public class Context
+	{
+		readonly IConfiguration _configuration;
+		readonly string _connectionString;
+
+		public Context(IConfiguration configuration)
+		{
+			_configuration = configuration;
+			_connectionString = _configuration.GetConnectionString("connection");
+		}
+
+		public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+	}
+}
